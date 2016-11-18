54da98845f4633d3d5bca5fc84fc4ea94920c209
Sharon Isabel Torres Robles <sharontorres50@gmail.com>
2016-11-18 08:45:58 -0600

    correccion

--- calculator.py
+++ calculator.py
@@ -3,21 +3,16 @@ scrip para calcular la suma de una lista de numeros
 """
 
 
-class CalculatorClass:
-    
-    def _init_(self,lista):
-        self.lista=lista
+class CalculatorClass:  
     """
     se define la variable lista
     """
-
     def sum(self,lista):
         sum=0
         for i in range(0,len(lista)):
             sum=sum+lista[i]
         return sum
-    """
-       
+    """       
        se crea el metodo agregando una sentencia
        en el cual se retornara el valor obtenido
        de la suma de los  numeros 
