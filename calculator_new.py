3b7beb0783b3398c86b737225c38ecb5d17dcb86
Sharon Isabel Torres Robles <sharontorres50@gmail.com>
2016-11-22 12:25:23 -0600

    Corregido

---
+++ calculator_new.py
@@ -0,0 +1,25 @@
+"""
+scrip para calcular la suma de una lista de numeros
+"""
+
+
+class CalculatorClass:
+    
+    def _init_(self, lista):
+        self.lista = lista
+    """
+    se define la variable lista
+    """
+
+    def sum(self, lista):
+        sum = 0
+        for num in range(0, len(lista)):
+            sum = sum + lista[num]
+        return sum
+    """
+       
+       se crea el metodo agregando una sentencia
+       en el cual se retornara el valor obtenido
+       de la suma de los  numeros 
+       """
+        
---
+++ primes_new.py
@@ -0,0 +1,33 @@
+"""
+script para comprobar si el numero es primo o no
+"""
+
+
+class PrimeClass(object):
+
+    def _init_(self, num_int):
+        self.num_int = num_int
+       
+        
+    
+    """
+    se instancia el objeto que en este caso es la variable num_int
+    """
+    def is_prime(self, num_int):
+        divisor = 2
+        prim = True
+
+        while((prim) and (divisor!=num_int)):
+            if (num_int%divisor==0):
+                return False
+        return True
+    """
+    se declara la variable divisor la cual tomara el valor entero de 2,
+    posteriormente se declara otra variable llamada prim dando referencia
+    a que si el numero dado es primo,despues se declara una sentencia para
+    comprobar que el numero sea primo, sino cumple nos devolvera un False.
+
+    """
+    
+
+       
